f81ee2afed77f447c47fcef1b3482ab2003f3969
krishkh <krishkhanna00@gmail.com>
2025-03-12 21:32:27 +0530

    made careers page responsive

--- src/components/careers/HeroSection.tsx
+++ src/components/careers/HeroSection.tsx
@@ -2,14 +2,14 @@
 
 export default function HeroSection() {
   return (
-    <section className="border-4 border-primary p-8">
-      <div className="text-center space-y-6">
-        <div className="text-4xl md:text-7xl font-black text-primary pb-12">
+    <section className="border-4 sm:border-4 border-primary p-4 sm:p-6 md:p-8">
+      <div className="text-center space-y-3 sm:space-y-4 md:space-y-6">
+        <div className="text-2xl sm:text-3xl md:text-5xl lg:text-7xl font-black text-primary pb-4 sm:pb-6 md:pb-12">
           <h1 className="pb-1">Join Our Team of</h1> Digital Innovatiors
         </div>
 
-        <div className="space-y-4 text-primary text-3xl font-medium">
-          <p className="px-32 font-medium">
+        <div className="space-y-2 sm:space-y-3 md:space-y-4 text-primary text-lg sm:text-xl md:text-2xl lg:text-3xl font-medium">
+          <p className="px-4 sm:px-8 md:px-16 lg:px-32 font-medium">
             Be part of a dynamic team that&apos;s shaping the future of digital
             marketing. We&apos;re always looking for passionate individuals who
             bring fresh perspective and innovative ideas
